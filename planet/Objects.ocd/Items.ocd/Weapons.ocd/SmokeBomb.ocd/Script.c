b2270246b2830be746d5ba39850f92341ea1f8be
WinExploder <m_adam.emden@yahoo.de>
2015-07-26 20:44:47 +0200

    Added definition and graphics for smokebomb

---
+++ planet/Objects.ocd/Items.ocd/Weapons.ocd/SmokeBomb.ocd/Script.c
@@ -0,0 +1,13 @@
+/*--- Rock ---*/
+
+protected func Hit(x, y)
+{
+	StonyObjectHit(x,y);
+	return true;
+}
+
+local Collectible = 1;
+local Name = "$Name$";
+local Description = "$Description$";
+local Rebuy = true;
+local Plane = 450;

0ad5e329b101c8ddc83c9a725d6aaada2b906675
Armin <arminnessie@gmail.com>
2015-08-22 22:05:27 +0200

    Fix stalactites water sources being out of solid material

--- planet/Objects.ocd/Environment.ocd/Stalactite1.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Stalactite1.ocd/Script.c
@@ -2,18 +2,13 @@
 	Stalactite1
 	Hangs from the ceiling
 
-	@author Armin
+	@author Armin, Win
 */
 
 private func Initialize()
 {
-	SetGraphics(Format("%d",Random(5)));
-	
-	SetProperty("MeshTransformation", Trans_Mul(Trans_Scale(600+Random(900)), Trans_Rotate(-25+Random(50),0,1,0)));
-	
-	/*var tinys = CreateObject(TinyStalactite, 0, 0);
-	tinys->SetProperty("MeshTransformation", Trans_Mul(Trans_Scale(1000), Trans_Rotate(180,0,0,1)));
-	tinys->SetParent(this);*/
+	SetGraphics(Format("%d", Random(6)));
+	SetProperty("MeshTransformation", Trans_Mul(Trans_Scale(600 + Random(900)), Trans_Rotate(-25 + Random(50), 0, 1, 0)));
 }
 
 private func Hit()
@@ -23,76 +18,72 @@ private func Hit()
 	return true;
 }
 
-private func SetStalagmite()
-{
-	SetProperty("MeshTransformation", Trans_Mul(Trans_Scale(1000), Trans_Rotate(180,0,0,1)));
-}
-
 public func Place(int amount, proplist rectangle, proplist settings)
 {
 	// Only allow definition call.
-	if (this != Stalactite1) 
+	if (this != Stalactite1)
 		return;
 	// Default parameters.
-	if (!settings) 
+	if (!settings)
 		settings = { size = [100, 100] };
-	if (!settings.size) 
+	if (!settings.size)
 		settings.size = [100, 100];
 	var loc_area = nil;
-	if (rectangle) 
+	if (rectangle)
 		loc_area = Loc_InRect(rectangle);
 	var loc_background = Loc_Tunnel();
 	if (settings.underground)
 		loc_background = Loc_Tunnel();
-		
-	var stalactites = [];	
+
+	var stalactites = [];
 	for (var i = 0; i < amount; i++)
 	{
 		var size = RandomX(settings.size[0], settings.size[1]);
 		var loc = FindLocation(loc_background, Loc_Not(Loc_Liquid()), Loc_Wall(CNAT_Right), Loc_Wall(CNAT_Top), loc_area);
 		if (!loc)
 			continue;
+		var mat = MaterialName(GetMaterial(loc.x, loc.y-30));
+		CreateStalactite(loc.x, loc.y + 25, mat);
 
-		var stalactite = CreateObject(Stalactite1);
-		stalactite->SetPosition(loc.x, loc.y + 25);
-		var tinys = CreateObject(TinyStalactite, 0, 0);
-		tinys->SetPosition(loc.x, loc.y+4);
-		tinys->SetParent(stalactite);
-		stalactite->SetCon(size);
-		// todo stalactite->AdjustPosition();
-		var mat = MaterialName(stalactite->GetMaterial(0, -stalactite->GetObjHeight()/2));
-		//stalactite->Message("Material: %s|Minus: %d", mat, stalactite->GetObjHeight()/2);
-		if (mat == "Ice")
+		// If possible, try to create a stalagmite below the stalactite.
+		if (Random(3))
 		{
-			stalactite->SetClrModulation(RGBa(157,202,243,160));
-			tinys->SetClrModulation(RGBa(157,202,243,160));
+			var xy = FindConstructionSite(Stalactite1, loc.x, loc.y+60);
+			if (xy)
+				if (MaterialName(GetMaterial(loc.x, xy[1] + 2 + 30)) == mat)
+					CreateStalactite(loc.x, xy[1]-28, mat, true);
 		}
-		else
-		{
-			stalactite->SetClrModulation(GetAverageTextureColor(mat));
-			tinys->SetClrModulation(GetAverageTextureColor(mat));
-			tinys->DrawWaterSource();
-		}
-		if (!Random(2))
-		{
-			var xy;
-			if (xy = FindConstructionSite(Stalactite1, loc.x, loc.y+60));
-			{
-				var stalactite2 = CreateObject(Stalactite1, loc.x, xy[1]-28);
-				stalactite2->SetR(180);
-				if (mat == "Ice")
-				{
-					stalactite2->SetClrModulation(RGBa(157,202,243,160));
-				}
-				else
-				{
-					stalactite2->SetClrModulation(GetAverageTextureColor(mat));
-				}
-			}
-		}
-		PushBack(stalactites, stalactite);	
 	}
 	return stalactites;
+}
+
+private func CreateStalactite(int x, int y, string mat, bool stalagmite)
+{
+	var stalactite = CreateObject(Stalactite1, x, y);
+	var tinys = CreateObject(TinyStalactite, x, y - 22);
+	tinys->SetChild(stalactite);
+
+	// Ice stalactites are transparent and never use water sources.
+	if (mat == "Ice")
+	{
+		stalactite->SetClrModulation(RGBa(157, 202, 243, 160));
+		tinys->SetClrModulation(RGBa(157, 202, 243, 160));
+	}
+	else
+	{
+		stalactite->SetClrModulation(GetAverageTextureColor(mat));
+		tinys->SetClrModulation(GetAverageTextureColor(mat));
+		if (!stalagmite && Random(2))
+			tinys->DrawWaterSource();
+	}
+
+	if (stalagmite)
+	{
+		stalactite->SetR(180);
+		tinys->SetPosition(x, y + 22);
+		tinys->SetR(180);
+	}
+	// todo stalactite->AdjustPosition();
 }
 
 local Name = "$Name$";
--- planet/Objects.ocd/Environment.ocd/Stalactite1.ocd/TinyStalactite.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Stalactite1.ocd/TinyStalactite.ocd/Script.c
@@ -2,37 +2,49 @@
 	TinyStalactite
 	Covers up the base of stalactites
 
-	@author Armin
+	@author Armin, Win
 */
 
 private func Initialize()
 {
 }
 
-private func SetParent(object target)
+private func SetChild(object target)
 {
-	parent = target;
-	AddEffect("Parent",this,1,1,this);
+	child = target;
+	AddEffect("Parent", this, 1, 10, this);
 }
 
 private func FxParentTimer(object target, effect, int time)
 {
+	if (!child)
+	{
+		child = CreateObject(Stalactite1);
+	}
 	//SetPosition(parent->GetX(), parent->GetY());
 }
 
 private func DrawWaterSource()
 {
-	var x = GetX();
-	var y = GetY();
-	for (var i = 0; i < RandomX(30,140); i++)
+	var xold = GetX();
+	var yold = GetY()-8;
+	var xnew = GetX();
+	var ynew = GetY()-8;
+	for (var i = 0; i < RandomX(30, 140); i++)
 	{
-		DrawMaterialQuad("Water", x,y, x+1,y, x+1,y+1, x,y+1);
-		y--;
-		x = x + RandomX(-1, 1);
+		ynew--;
+		xnew = xold + RandomX(-1, 1);
+		if (GBackSemiSolid(xnew-GetX(), ynew-GetY()))
+			DrawMaterialQuad("Water", xold,yold, xold+1,yold, xold+1,yold+1, xold,yold+1);	
+		else
+			return;
+		xold = xnew;
+		yold = ynew;
 	}
 }
 
 local Name = "$Name$";
 local Description = "$Description$";
-local parent;
 local Plane = 500;
+local child;
+local mat;

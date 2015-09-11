de9f7ed0bcf3d01ec5c8de75c37e81cfd60662dc
David Dormagen <czapper@gmx.de>
2015-09-11 18:13:08 +0200

    cleaned up boiling lava and boiling acid rule
    Made them adhere to the C4S guidelines, renamed some IDs and made some stuff in scripts more readable.

--- planet/Objects.ocd/Environment.ocd/Disasters.ocd/BoilingAcid.ocd/Script.c
+++ planet/Objects.ocd/Environment.ocd/Disasters.ocd/BoilingAcid.ocd/Script.c
@@ -5,41 +5,34 @@
 	@author 
 */
 
+#include BoilingLava
+
 local Name = "$Name$";
 local Description = "$Description$";
 
-local intensity;
+// Magic number by which the total map size (in pixels) is divided to get the amount of tries per frame.
+local intensity_quotient = 50000;
 
-protected func Initialize()
+private func Boiling()
 {
-	SetPosition(0,0);
-	intensity = (LandscapeWidth() * LandscapeHeight())/50000;
-	
-	AddEffect("SearchAcid",this,1,1,this);
-}
-
-func FxSearchAcidTimer(object target, effect, int time)
-{
-	for(var i = 0; i < intensity; i++)
-	
+	for (var i = 0; i < intensity; i++)
 	{
 		// Checks if there is a deep enough pool of acid at a random location of the map, then creates spawner at a random depth into the pool
-		var xRand = Random(LandscapeWidth());	
-		var yRand = Random(LandscapeHeight());
-		var mat = MaterialName(GetMaterial(xRand, yRand));
-		var randDepth = RandomX(30, 100);
-		var depthCheckMat = MaterialName(GetMaterial(xRand, yRand+randDepth));
+		var x_rand = Random(LandscapeWidth());	
+		var y_rand = Random(LandscapeHeight());
+		var mat = MaterialName(GetMaterial(x_rand, y_rand));
+		var random_depth = RandomX(30, 100);
+		var depth_check_mat = MaterialName(GetMaterial(x_rand, y_rand + random_depth));
 
-		if(mat == "Acid")
-			if(depthCheckMat == "Acid")
-				if(PathFree(xRand, yRand, xRand, yRand+randDepth))
-				{	
-					var nearbySpawner = FindObject(Find_Distance(RandomX(80,100), xRand, yRand), Find_ID(AcidSpawner));
-				
-					if(nearbySpawner == nil)
-					{
-						CreateObject(AcidSpawner, xRand, yRand+randDepth);
-					}
+		if (mat == "Acid" && depth_check_mat == "Acid")
+			if (PathFree(x_rand, y_rand, x_rand, y_rand + random_depth))
+			{	
+				var nearbySpawner = FindObject(Find_Distance(RandomX(80, 100), x_rand, y_rand), Find_ID(BoilingAcid_Spawner));
+			
+				if (nearbySpawner == nil)
+				{
+					CreateObject(BoilingAcid_Spawner, x_rand, y_rand + random_depth);
 				}
+			}
 	}
 }

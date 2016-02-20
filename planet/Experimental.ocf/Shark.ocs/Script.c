8ec1a7fe857b3cff5d77a55cd5fbf1327df4bd41
Armin Schäfer <arminnessie@gmail.com>
2016-02-20 14:48:12 +0100

    Shark update. Fix animation when attacking.

--- planet/Experimental.ocf/Shark.ocs/Script.c
+++ planet/Experimental.ocf/Shark.ocs/Script.c
@@ -1,6 +1,23 @@
 func Initialize()
 {
-	Shark->Place(3);
-	Seaweed->Place(16);
-	Coral->Place(3);
+	Shark->Place(2);
+	Squid->Place(1);
+	Seaweed->Place(71);
+	Coral->Place(4);
+}
+
+private func InitializePlayer(iPlr)
+{
+	SetFoW(0, iPlr);
+	var crew = GetCrew(iPlr);
+	crew->SetPosition(10,300);
+	crew->CreateContents(Shovel);
+}
+
+func RelaunchPlayer(int iPlr)
+{
+	var pclonk = CreateObject(Clonk, 10,300, iPlr);
+	pclonk->MakeCrewMember(iPlr);
+	SetCursor(iPlr, pclonk);
+	pclonk->CreateContents(Shovel);
 }
--- planet/Experimental.ocf/Shark.ocs/Shark.ocd/Script.c
+++ planet/Experimental.ocf/Shark.ocs/Shark.ocd/Script.c
@@ -3,149 +3,362 @@
 	Very strong water animal.
 */
 
-#include Fish
+local attacking;
+// 0 = Idle.
+// 1 = Moving back to his old position and waiting a bit before attacking again.
+// 2 = Charging for Clonk.
+local BiteStrength = 25;
+local walking, swimming, swim_animation, base_transform;
 
-local SwimMaxAngle = 5;
-local SwimMaxSpeed = 45;
-// Default additional scaling for the mesh.
-local BaseScale = +200;
-// Damage per bite.
-local BiteStrength = 20;
-local wall_vision_range = 160;
-local hunger;
-
-func Construction()
+private func Construction()
 {
-	hunger = 60;
-	AddTimer("MoreHunger", 80);
-	// Set base transform (can be overwritten by e.g. Place()).
-	SetYZScale(1000);
-	_inherited(...);
+	StartGrowth(15);
+	SetAction("Swim");
+	if (!Random(2))
+		SetComDir(COMD_Right);
+	else
+		SetComDir(COMD_Left);
+	if (GetAlive())
+		AddTimer(this.Activity, 10);
+
+	return _inherited(...);
 }
 
-func MoreHunger()
+private func Attack(object target, int x, int y)
 {
-	++hunger;
-	if (hunger > 100) hunger = 100;
-}
-
-func InitFuzzyRules()
-{
-	brain = FuzzyLogic->Init();
-	
-	// ACTION SETS
-	brain->AddSet("swim", "sharp_left", [[-2 * SwimMaxAngle, 1], [-SwimMaxAngle, 0], [SwimMaxAngle, 0]]);
-	brain->AddSet("swim", "left", [[-SwimMaxAngle, 1], [-SwimMaxAngle/2, 0], [SwimMaxAngle, 0]]);
-	brain->AddSet("swim", "straight", [[-5, 0], [0, 1], [5, 0]]);
-	brain->AddSet("swim", "right", [[-SwimMaxAngle, 0], [SwimMaxAngle/2, 0], [SwimMaxAngle, 1]]);
-	brain->AddSet("swim", "sharp_right", [[-SwimMaxAngle, 0], [SwimMaxAngle, 0], [2 * SwimMaxAngle, 1]]);
-	
-	brain->AddSet("speed", "slow", [[0, 1], [SwimMaxSpeed/2, 0], [SwimMaxSpeed, 0]]);
-	brain->AddSet("speed", "fast", [[0, 0],  [SwimMaxSpeed/2, 0], [SwimMaxSpeed, 1]]);
-	
-	// RULE SETS
-	var directional_sets = ["food"];
-	
-	for (var set in directional_sets)
+	if (attacking < 2)
+		return 1;
+	var dis = Distance(GetX(), GetY(), target->GetX(), target->GetY());
+	if (dis > 30)
 	{
-		brain->AddSet(set, "left", [[-VisionMaxAngle, 1], [0, 0], [VisionMaxAngle, 0]]);
-		brain->AddSet(set, "straight", [[-5, 0], [0, 1], [5, 0]]);
-		brain->AddSet(set, "right", [[-VisionMaxAngle, 0], [0, 0], [VisionMaxAngle, 1]]);
+		var current_angle=Angle(GetX(), GetY(), target->GetX(), target->GetY());
+		if (current_angle < 0) current_angle = 360 + current_angle;
+		var t = current_angle - 270;
+		var t2 = Cos(t, 90) - 90; // Expand the areas around 0deg and 180deg a bit so you see fish from the side more
+		this.MeshTransformation = Trans_Mul(Trans_Rotate(t, 0, 0, 1), Trans_Rotate(t2, 1, 0, 0));
 	}
-	
-	var proximity_sets = ["food_range"];
-	var middle = VisionMaxRange / 2;
-	
-	for (var set in proximity_sets)
+	this->SetCommand("MoveTo", target);
+	if (dis < 20)
 	{
-		brain->AddSet(set, "far", [[middle, 0], [VisionMaxRange, 1], [VisionMaxRange, 1]]);
-		brain->AddSet(set, "medium", [[0, 0], [middle, 1], [VisionMaxRange, 0]]);
-		brain->AddSet(set, "close", [[0, 1], [0, 1], [middle, 0]]);
+		// Victim near. Bite him.
+		SetAction("Bite");
+		Sound("Animals::Fish::Munch*");
+		if (target->GetAlive())
+			target->DoEnergy(-BiteStrength);
+		DoEnergy(BiteStrength);
+
+		// Mission succesful. Swim back to the initial position.
+		SetCommand("None");
+		this["MeshTransformation"] = Trans_Identity();
+		this->SetCommand("MoveTo", nil, x, y);
+
+		// Let the Clonk in peace for a few seconds.
+		attacking = 1;
+		ScheduleCall(this, "ResetHunger", 300);
+		return 1;
 	}
-	
-	brain->AddSet("left_wall", "close", [[0, 1], [0, 1], [wall_vision_range/2, 0]]);
-	brain->AddSet("right_wall", "close", [[0, 1], [0, 1], [wall_vision_range/2, 0]]);
-	brain->AddSet("wall_range", "close", [[0, 1], [0, 1], [wall_vision_range, 0]]);
-	
-	brain->AddSet("hunger", "low", [[0, 1], [0, 1], [75, 0]]);
-	brain->AddSet("hunger", "high", [[25, 0], [100, 1], [100, 1]]);
-	
-	// RULES
-	brain->AddRule(brain->And("hunger=high", "food=right"), "swim=right");
-	brain->AddRule(brain->And("hunger=high", "food=left"), "swim=left");
-	brain->AddRule("hunger=high", "speed=fast");
-	brain->AddRule(brain->Or("wall_range=close", "hunger=low"), "speed=slow");
-	brain->AddRule(brain->And("left_wall=close", brain->Not("right_wall=close")), "swim=sharp_right");
-	brain->AddRule("right_wall=close", "swim=sharp_left");
+	ScheduleCall(this, "Attack", 5, 0, target, x, y);
 }
 
-func UpdateVision()
+private func Turn()
 {
-	brain->Fuzzify("hunger", hunger);
-	UpdateVisionFor("food", "food_range", FindObjects(Find_Distance(VisionMaxRange), Find_OCF(OCF_Alive), Find_Func("IsPrey"), Find_NoContainer(), Sort_Distance()), true);
-	UpdateWallVision();
+	SetComDir(COMD_None);
+	SetXDir(0);
+	SetYDir(0);
+	SetAction("Turn");
+	if (GetDir() == DIR_Left)
+	{
+		SetDir(DIR_Right);
+		SetComDir(COMD_Right);
+	}
+	else
+	{
+		SetDir(DIR_Left);
+		SetComDir(COMD_Left);
+	}
 }
 
-func DoEat(object obj)
+private func Activity()
 {
-	PlayAnimation("Bite");
-	BiteEffect();
-	var len = GetAnimationLength("Bite");
-	PlayAnimation("Bite", 5,  Anim_Linear(0, 0, len, 36, ANIM_Remove), Anim_Const(1000));
-	if (obj->GetAlive())
-		obj->DoEnergy(-BiteStrength);
-	hunger -= 10;
-	if (hunger < 0) hunger = 0;
-	//CastParticles("MaterialParticle", 10, 10, 0, 0, 10, 20, RGB(200, 5, 5), RGB(200, 5, 5));
+	if (swimming)
+	{
+		if (!attacking)
+		{
+			var target = FindObject(Find_ID(Clonk), Find_Distance(400), Find_Action("Swim"));
+			if (target)
+			{
+				attacking = 2;
+				Attack(target, GetX(), GetY());
+				SetAction("FastSwim");
+				return 1;
+			}
+
+			// Avoid surface.
+			if (!GBackLiquid(0, -8) && GetAction() != "Walk") 
+			return SetComDir(BoundBy(GetComDir(), COMD_DownRight, COMD_DownLeft));
+
+			// Wall?
+			if (GetContact(-1))
+				Turn();
+
+			// Do nothing.
+			if (Random(2))
+				return 1;
+
+			// Turn around.
+			if (!Random(40))
+				Turn();
+
+			// Avoid other sharks.
+			var obj = FindObject(Find_ID(Shark), Find_InRect(-100, -25, 200, 50), Find_NoContainer(), Find_Exclude(this));
+			if (obj)
+			{
+				if (obj->GetY() < GetY())
+					SetComDir(BoundBy(GetComDir(), COMD_DownRight, COMD_DownLeft));
+				else if (GetComDir() == COMD_Right)
+					SetComDir(COMD_UpRight);
+				else
+					SetComDir(COMD_UpLeft);
+			}
+		}
+	}
+	else if (walking)
+	{
+		// PANIC WHERE THE WATER AT
+		var rx = RandomX(10, 25);
+		if (!Random(2)) rx *= -1;
+		
+		var has_water = false;
+		for (var y = 0; y <= 12; y += 4)
+		{
+			if (!GBackLiquid(rx, y)) continue;
+			has_water = true;
+			break;
+		}
+		
+		if (has_water)
+		{
+			if (rx < 0) SetComDir(COMD_Left);
+			else SetComDir(COMD_Right);
+			
+			if (!Random(2))
+				DoJump();
+			return true;
+		}
+		else
+		{
+			if (!Random(2))
+			{
+				if (!Random(2)) SetComDir(COMD_Left);
+				else SetComDir(COMD_Right);
+			}
+			if (!Random(3))
+				DoJump();
+		}
+	}
+	return 1;
+}
+
+private func Place(int amount, proplist rectangle, proplist settings)
+{
+	var max_tries = 2 * amount;
+	var loc_area = nil;
+	if (rectangle) loc_area = Loc_InArea(rectangle);
+	var f;
 	
-	DoEnergy(BiteStrength);
+	while ((amount > 0) && (--max_tries > 0))
+	{
+		var spot = FindLocation(Loc_Material("Water"), Loc_Space(20), loc_area);
+		if (!spot) continue;
+		
+		f = CreateObjectAbove(this, spot.x, spot.y, NO_OWNER);
+		if (!f) continue;
+		// Randomly add some large/slim fish
+		if (Random(3))
+		{
+			// There are naturally smaller and larger fishes. Fishes above around 150 can be clipped, so stay below that.
+			f->SetCon(RandomX(75, 140));
+			// make sure the smaller ones don't grow larger any more
+			f->StopGrowth(); 
+			// Slim fish.
+			if (f->GetCon() > 100 || !Random(3))
+				f->SetYZScale(1000 - Random(300));  
+		}
+		
+		if (f->Stuck())
+		{
+			f->RemoveObject();
+			continue;
+		}
+		--amount;
+	}
+	return f; // return last created fish
 }
 
-private func BiteEffect()
+private func SetYZScale(int new_scale)
 {
-	Sound("Animals::Fish::Munch*");
+	base_transform = Trans_Scale(1000, new_scale, new_scale);
+	return true;
+}
+
+private func Death()
+{
+	RemoveTimer(this.UpdateSwim);
+	RemoveTimer(this.Activity);
+	this.MeshTransformation = Trans_Rotate(160 + Random(41), 1, 0, 0);
+	if (base_transform) this.MeshTransformation = Trans_Mul(base_transform, this.MeshTransformation);
+	StopAnimation(swim_animation);
+	// Decay();
+	this.Collectible = true;
+	
+	// maybe respawn a new fish if roe is near
+	var roe = FindObject(Find_Distance(200), Find_ID(FishRoe));
+	if (roe)
+		roe->Hatch(GetID());
+	
+	return _inherited(...);
+}
+
+private func DoJump()
+{
+	SetAction("Jump");
+	Sound("Hits::SoftTouch*");
+	
+	var x_dir = RandomX(ActMap.Jump.Speed/2, ActMap.Jump.Speed);
+	if (GetComDir() == COMD_Left) x_dir *= -1;
+	var y_dir = -RandomX(ActMap.Jump.Speed/3, ActMap.Jump.Speed);
+	SetSpeed(x_dir, y_dir);
+}
+
+private func StartWalk() 
+{
+	swim_animation = PlayAnimation("Swim", 5, Anim_Linear(0, 0, len, 100, ANIM_Loop), Anim_Const(500));
+	if (GBackLiquid())
+	{
+		SetAction("Swim");
+		return;
+	}
+	walking = true;
+	swimming = false;
+	
+	var len = GetAnimationLength("Swim");
+	var pos = GetAnimationPosition(swim_animation);
+	SetAnimationPosition(swim_animation, Anim_Linear(pos, 0, len, 10, ANIM_Loop));
+	this.MeshTransformation = Trans_Mul(Trans_Rotate(90 + RandomX(-10, 10), 1, 0, 0), base_transform);
+
+	ResetHunger();
+}
+
+private func StartSwim()
+{
+	StopAnimation(swim_animation);
+	swimming = true;
+	walking = false;
+	this["MeshTransformation"] = Trans_Identity();
+}
+
+private func StartJump()
+{
+	if (GBackLiquid())
+	{
+		SetAction("Swim");
+		return;
+	}
+	swimming = false;
+	walking = false;
+}
+
+private func ResetHunger()
+{
+	attacking = 0;
 }
 
 local ActMap = {
 Swim = {
 	Prototype = Action,
 	Name = "Swim",
+	Procedure = DFA_SWIM,
+	Speed = 100,
+	Accel = 64,
+	Decel = 16,
+	Length = 52,
+	Delay = 10,
 	Directions = 2,
+	FlipDir = 1,
+	StartCall = "StartSwim",
+	Animation = "Swim"
+},
+FastSwim = {
+	Prototype = Action,
+	Name = "Swim",
+	Procedure = DFA_SWIM,
+	Speed = 370,
+	Accel = 64,
+	Decel = 16,
+	Length = 20,
+	Delay = 10,
+	Directions = 2,
+	FlipDir = 0,
+	Animation = "Swim"
+},
+Turn = {
+	Prototype = Action,
+	Name = "Turn",
 	Procedure = DFA_SWIM,
 	Speed = 100,
 	Accel = 16,
 	Decel = 16,
 	Length = 52,
 	Delay = 10,
-	FacetBase=1,
-	StartCall = "StartSwim",
-	Animation = "Swim"
+	Directions = 2,
+	FlipDir = 0,
+	NextAction = "Swim",
+	Animation = "Turn"
 },
 Bite = {
 	Prototype = Action,
 	Name = "Bite",
-	Directions = 2,
 	Speed = 100,
 	Accel = 16,
 	Decel = 16,
 	Length = 2,
 	Delay = 10,
-	FacetBase=1,
+	Directions = 2,
+	NextAction = "Swim",
 	StartCall = "StartBite",
 	Animation = "Bite"
 },
-Turn = {
+Walk = {
 	Prototype = Action,
-	Name = "Turn",
-	Directions = 2,
-	Speed = 100,
+	Name = "Walk",
+	Procedure = DFA_WALK,
+	Speed = 30,
 	Accel = 16,
 	Decel = 16,
-	Length = 5,
-	Delay = 10,
+	Length = 1,
+	Delay = 0,
 	FacetBase=1,
-	StartCall = "StartTurn",
-	Animation = "Turn"
+	Directions = 2,
+	FlipDir = 1,
+	NextAction = "Walk",
+	StartCall = "StartWalk",
+	InLiquidAction = "Swim",
+},
+Jump = {
+	Prototype = Action,
+	Name = "Jump",
+	Procedure = DFA_FLIGHT,
+	Speed = 30,
+	Accel = 16,
+	Decel = 16,
+	Length = 1,
+	Delay = 0,
+	FacetBase=1,
+	Directions = 2,
+	FlipDir = 1,
+	NextAction = "Jump",
+	StartCall = "StartJump",
+	InLiquidAction = "Swim",
 },
 Dead = {
 	Prototype = Action,

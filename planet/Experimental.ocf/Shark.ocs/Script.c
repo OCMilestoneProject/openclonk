6a80fd85158c4ed9e707339bd05a44e6001aa7d1
Armin Schäfer <arminnessie@gmail.com>
2016-07-03 19:44:49 +0200

    Shark: Fix one turning bug, it can not be big enough to cause a mesh bug when turning around; shark is a bit slower when attacking (hopefully it can not jump out of the water now)

--- planet/Experimental.ocf/Shark.ocs/Script.c
+++ planet/Experimental.ocf/Shark.ocs/Script.c
@@ -4,6 +4,11 @@ func Initialize()
 	Squid->Place(1);
 	Seaweed->Place(71);
 	Coral->Place(4);
+	Fern->Place(2);
+	Tree_Deciduous->Place(6);
+	Tree_Deciduous_Burned->Place(1);
+	SproutBerryBush->Place(3);
+	Butterfly->Place(1);
 }
 
 private func InitializePlayer(iPlr)
@@ -12,6 +17,8 @@ private func InitializePlayer(iPlr)
 	var crew = GetCrew(iPlr);
 	crew->SetPosition(10,300);
 	crew->CreateContents(Shovel);
+	crew->CreateContents(Hammer);
+	crew->CreateContents(Rock, 3);
 }
 
 func RelaunchPlayer(int iPlr)
